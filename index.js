258d276b51a850ad6ca9770da95645c87355879f
Vo Chanh Tin <tin.vo.4767@outlook.com>
2023-11-16 09:23:00 +0700

    done 2

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => res.render("index"));
 
 app.get("/task1.htm", (req, res) => res.render("task1"));
 
+app.get("/admin", (req, res) => {
+    res.render("index", {layout: "admin"});
+});
+
 app.engine("hbs",
     expressHbs.engine({
         layoutsDir: __dirname + "/views/layouts",
