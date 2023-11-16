2306f5e823f7c407d0c60e8a047baa32566c98e3
Vo Chanh Tin <tin.vo.4767@outlook.com>
2023-11-16 10:46:24 +0700

    done task 3

--- controllers/task3Controller.js
+++ controllers/task3Controller.js
@@ -1,7 +1,18 @@
 const controller = {};
 
 controller.show = (req, res) => {
-    res.render("task3");
+    let {categories} = require("../data");
+    let {products} = require("../data");
+
+    let productList = req.query.category ? products.filter((item) => item.category == req.query.category) : products;
+
+    // res.locals.categories = categories;
+    // res.locals.products = products;
+
+    res.render("task3", {
+        products: productList,
+        categories
+    });
 };
 
 module.exports = controller;
