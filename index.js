387a28055b54cde278324190cff51abe8fa54475
Vo Chanh Tin <tin.vo.4767@outlook.com>
2023-11-16 09:44:22 +0700

    delete index.htm and task.htm

--- index.js
+++ index.js
@@ -3,16 +3,6 @@ const expressHbs = require("express-handlebars");
 const app = express();
 const port = 3001;
 
-app.use(express.static(__dirname + "/html"));
-
-app.get("/", (req, res) => res.render("index"));
-
-app.get("/task1.htm", (req, res) => res.render("task1"));
-
-app.get("/admin", (req, res) => {
-    res.render("index", {layout: "admin"});
-});
-
 app.engine("hbs",
     expressHbs.engine({
         layoutsDir: __dirname + "/views/layouts",
@@ -23,5 +13,20 @@ app.engine("hbs",
 );
 
 app.set("view engine", "hbs");
+
+app.use(express.static(__dirname + "/html"));
+
+app.get("/", (req, res) => res.render("index"));
+
+app.get("/task1.htm", (req, res) => {
+    let {emotions} = require("./data");
+    res.locals.emotions = emotions;
+    res.render("task1");
+});
+
+app.get("/admin", (req, res) => {
+    res.render("index", {layout: "admin"});
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
